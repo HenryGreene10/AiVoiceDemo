4de4fa3c3e29778e42be36c62dbc4b9de1184646
Henry Greene <hxg204@nyu.edu>
2025-12-17 11:24:09 -0500

    tracking

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,15 @@
+import Script from "next/script"
+
 export default function Page() {
   return (
     <>
+      <Script id="ms-clarity" strategy="afterInteractive">
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "un0i0tws0o");`}
+      </Script>
       <header className="ea-nav">
         <div className="ea-nav-inner">
           <span className="ea-nav-title">EasyAudio</span>
